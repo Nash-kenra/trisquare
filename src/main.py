dad4e9285771648f45b2d8e4b1ca4578c1a55395
anandkenra <anand2work@gmail.com>
2023-09-12 05:36:48 -0500

    Load all data from main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from pulse.integration.fmpapi_to_db import FmpApiToDatabase
 
-#FmpApiToDatabase.load_index_companies()
+FmpApiToDatabase.load_index_companies()
 
-#FmpApiToDatabase.load_global_stocks()
+FmpApiToDatabase.load_global_stocks()
 
-#FmpApiToDatabase.load_historical_prices()
+FmpApiToDatabase.load_historical_prices()
 
 FmpApiToDatabase.load_daily_prices()
 
