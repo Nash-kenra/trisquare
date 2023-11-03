859d78590a866498efa04021eb0d9295dd7e6a7a
Vijaya-Kenra <vijayap.kota@gmail.com>
2023-11-03 11:45:13 -0500

    Changes for Company Recommendations API

--- src/pulse/fmpapi/company_analysis_api.py
+++ src/pulse/fmpapi/company_analysis_api.py
@@ -7,3 +7,10 @@ class Comp_Estimates(GetApi):
     def __init__(self, symbol)->None :
         query = "analyst-estimates/" + symbol +"?"
         super().__init__(query)
+
+class comp_recom(GetApi):
+# The class just need to fill in the details of the query string for particular company recommendations
+# data. The Base class takes care of all the execution of the rest of the functionality.
+    def __init__(self, symbol)->None:
+        query = "analyst-stock-recommendations/" + symbol +"?"
+        super().__init__(query)
