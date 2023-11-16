561ff6d8e5b285da8eaae469d7c06c5ca8e985a3
Vijaya-Kenra <vijayap.kota@gmail.com>
2023-11-15 18:26:31 -0600

    code refactored

--- src/pangea/routes/pulse_load.py
+++ src/pangea/routes/pulse_load.py
@@ -4,45 +4,24 @@ from pulse.integration.fmpapi_to_db import FmpApiToDatabase
 pulse_load = Blueprint('pulse_load',__name__)
 
 query_string_to_function_mapping = {
-        "indexCompanies":{
-            "api_name":FmpApiToDatabase.load_index_companies,
-            "message": "Loaded index companies data successfully"
-        }, 
-        "globalStocks":{
-            "api_name":FmpApiToDatabase.load_global_stocks,
-            "message": "Loaded global stocks data successfully"
-        }, 
-        "historicalPrices":{
-            "api_name":FmpApiToDatabase.load_historical_prices,
-            "message": "Loaded historical prices data successfully"
-        }, 
-        "dailyPrices":{
-            "api_name":FmpApiToDatabase.load_daily_prices,
-            "message": "Loaded daily prices data successfully"
-        }, 
-        "compEstimates":{
-            "api_name":FmpApiToDatabase.load_comp_estimates,
-            "message": "Loaded company estimates data successfully"
-        }, 
-        "compRatings":{
-            "api_name":FmpApiToDatabase.load_comp_ratings,
-            "message": "Loaded company ratings data successfully"
-        }, 
-        "compRecom":{
-            "api_name":FmpApiToDatabase.load_comp_recom,
-            "message": "Loaded company recommendations data successfully"
-        } 
-    }   
+    "indexCompanies": FmpApiToDatabase.load_index_companies,
+    "globalStocks": FmpApiToDatabase.load_global_stocks,
+    "historicalPrices": FmpApiToDatabase.load_historical_prices,
+    "dailyPrices": FmpApiToDatabase.load_daily_prices,
+    "compEstimates": FmpApiToDatabase.load_comp_estimates,
+    "compRatings": FmpApiToDatabase.load_comp_ratings,
+    "compRecom": FmpApiToDatabase.load_comp_recom,
+}
 
 @pulse_load.route('/pulse/load', methods=['GET'])
 def get_load_pulse_data():
     # Reading query parameter 'data' from request
     selected_data= request.args.get('data')    
-    function_mapping = query_string_to_function_mapping.get(selected_data)
+    function = query_string_to_function_mapping.get(selected_data)
     try:
-        if function_mapping:
-            function_mapping.get("api_name")()
-            response = jsonify({"message": function_mapping.get("message")}),200
+        if function:
+            function()           
+            response = jsonify({"message": f"Loaded {selected_data} data successfully"}),200
         else:
             response=jsonify({"error": "Bad Request ---Invalid data"}),400           
     except Exception as e:
