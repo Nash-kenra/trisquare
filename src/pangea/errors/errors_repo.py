d68171404976fab557571f7cec67b38cb62e39b8
anandkenra <anand2work@gmail.com>
2023-11-22 12:06:47 -0600

    Error handling in Pangea
    Error handling in Pangea

---
+++ src/pangea/errors/errors_repo.py
@@ -0,0 +1,6 @@
+{
+    "error_code": "E001",
+    "message": "Invalid input",
+    "description": "Please provide valid input parameters.",
+    "data": {"field": "username", "value": "invalid_value"}
+}
